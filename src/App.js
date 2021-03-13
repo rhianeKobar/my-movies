15b0b99a9be3ccb3b4c55d218210678dc6303822
rhianeKobar <shakira.r.salazar@gmail.com>
2021-03-13 19:30:47 +0000

    add and remove from faves

--- src/App.js
+++ src/App.js
@@ -1,40 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import MovieList from './components/movieList';
-import Heading from './components/Heading';
-import Search from './components/Search';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import Heading from "./components/Heading";
+import Search from "./components/Search";
+import addFave from "./components/addFaves";
+import removeFave from "./components/removeFave";
 
 //http://www.omdbapi.com/s=funny&apikey=321a95e5
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searching, setSearching] = useState('');
-  //const [faves, setFaves] = useState([]);
+  const [searching, setSearching] = useState("");
+  const [faves, setFaves] = useState([]);
 
-  const getMovies = async(searching) =>{
+  const getMovies = async (searching) => {
     const url = `http://www.omdbapi.com/?s=${searching}&apikey=321a95e5`;
     const response = await fetch(url);
-    const jsonResponse= await response.json();
-    if(jsonResponse.Search){
+    const jsonResponse = await response.json();
+    if (jsonResponse.Search) {
       setMovies(jsonResponse.Search);
     }
-    
-    
-  }
+  };
 
   useEffect(() => {
     getMovies(searching);
-  },[searching]);
+  }, [searching]);
+
+  const addToFaves = (movie) => {
+    const newFaves = [...faves, movie];
+    console.log("ah gat clicked!");
+    setFaves(newFaves);
+  };
+
+  const removeFromFaves = (movie) => {
+    const newFaveList = faves.filter((fave) => fave.imdbID !== movie.imdbID);
+    setFaves(newFaveList);
+  };
+
+  useEffect(()=>{
+    addToFaves(movies)
+  },[])
   
+  useEffect(()=>{
+    removeFromFaves(faves)
+  },[])
+
   return (
     <div className="container-fluid mm-app">
-      <div className="row m-4">
+      <div className="row d-flex align-items-center mt-4 mb-4">
         <Heading heading="Movies" />
-        <Search searching={searching} setSearching={setSearching}/>
+        <Search searching={searching} setSearching={setSearching} />
       </div>
       <div className="row">
-        <MovieList movies={movies} />
+        <MovieList
+          movies={movies}
+          favourite={addFave}
+          handleClick={addToFaves}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <Heading heading="Faves" />
+      </div>
+      <div className="row">
+      <MovieList
+          movies={faves}
+          favourite={removeFave}
+          handleClick={removeFromFaves}
+        />
       </div>
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
 function MovieList(props){
+  const FavouriteThis = props.favourite;
     return(
         <>
             {
                 props.movies.map((movie,idx) =>{
                     return (
-                      <div className=" d-flex justify-content-start m-3">
+                      <div className="d-flex justify-content-start m-3 image-container">
                         <img
                           src={movie.Poster}
                           alt={movie.Title}
-                          key={movie.idx}
+                          key={idx}
                         ></img>
+                        <div onClick={()=> props.handleClick(movie)} className="overlay d-flex align-items-center justify-content-center">
+                          <FavouriteThis />
+                        </div>
                       </div>
                     );
                 })
